7cde1fdc2e94c516ca3c41002caa46d6fb4199eb
Nidhinsha <nidhinsha64@gmail.com>
2023-03-01 21:42:14 +0530

    custom hooks developed

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,43 +1,38 @@
-import React, { Fragment } from 'react'
+import { Fragment } from 'react';
 
-function Table({data,config,keyFn}) {
+function Table({ data, config, keyFn }) {
+  const renderedHeaders = config.map((column) => {
+    if (column.header) {
+      return <Fragment key={column.label}>{column.header()}</Fragment>;
+    }
 
-    const renderedHeader = config.map((column)=>{
+    return <th key={column.label}>{column.label}</th>;
+  });
 
-        if(column.header){
-            return <Fragment key={column.label}>{column.header()}</Fragment> 
-        }
+  const renderedRows = data.map((rowData) => {
+    const renderedCells = config.map((column) => {
+      return (
+        <td className="p-2" key={column.label}>
+          {column.render(rowData)}
+        </td>
+      );
+    });
 
-        return <th key={column.label}>{column.label}</th>
-    })
+    return (
+      <tr className="border-b" key={keyFn(rowData)}>
+        {renderedCells}
+      </tr>
+    );
+  });
 
-    const renderdRows = data.map((rowData)=>{
-
-        const renderedCells = config.map((column)=>{
-            return <td className='p-2' key={column.label}>{column.render(rowData)}</td>
-        })
-
-        return (
-            <tr key={keyFn(rowData)} className='border-b'>
-                {renderedCells}
-                
-            </tr>
-        )
-    })
   return (
-    <div>
-     <table className='table-auto border-spacing-2'>
-        <thead>
-            <tr className='border-b-2'>
-                {renderedHeader}
-            </tr>
-        </thead>
-        <tbody>
-            {renderdRows}
-        </tbody>
-     </table>
-    </div>
-  )
+    <table className="table-auto border-spacing-2">
+      <thead>
+        <tr className="border-b-2">{renderedHeaders}</tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
 }
 
-export default Table
+export default Table;
