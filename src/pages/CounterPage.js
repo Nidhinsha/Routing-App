8aaf5f5fd80940a44a6b1c42d5a50936b910a648
Nidhinsha <nidhinsha64@gmail.com>
2023-03-02 13:37:20 +0530

    before ysing immit

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,37 +1,110 @@
 
-import { useState } from 'react';
+import { useReducer } from 'react';
 import Button from '../components/Button';
 import Panel from '../components/Panel';
 
+// step 1 add new constant action type string
+// step 2 add a dispatch 
+// step 3 add a new statement in your reducer
+
+const INCREMENT_COUNT = 'increment'
+const SET_VALUE_TO_ADD = 'change-value-to-add'
+const DECREMENT_COUNT = 'decrement'
+const ADD_VALUE_TO_COUNT ='add_value_to_count'
+
+const reducer = (state,action)=>{
+
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      return{
+        ...state,
+        count:state.count + 1
+      } 
+    
+    case DECREMENT_COUNT:
+      return{
+        ...state,
+        count:state.count-1
+      }
+     
+
+    case SET_VALUE_TO_ADD:
+      return{
+        ...state,
+        valueToAdd:action.payload
+      } 
+    
+      case ADD_VALUE_TO_COUNT:
+        return {
+          ...state,
+          count:state.count + state.valueToAdd,
+          valueToAdd: 0
+        }
+  
+    default:
+      return state
+  }
+  // if (action.type === INCREMENT_COUNT) { 
+    // return{
+    //   ...state,
+    //   count:state.count + 1
+    // } 
+  // }
+  // if (action.type === SET_VALUE_TO_ADD) { 
+    // return{
+    //   ...state,
+    //   valueToAdd:action.payload
+    // } 
+  // }
+
+  // return state
+}
 
 function CounterPage({initialCount}) {
  
-  const [count,setCount] = useState(initialCount)
+  // const [count,setCount] = useState(initialCount)
 
-  const [valueToAdd,setValueToAdd] = useState(0)
+  // const [valueToAdd,setValueToAdd] = useState(0)
+
+  const [state,dispatch] = useReducer(reducer,{
+    count:initialCount,
+    valueToAdd:0
+  })
 
   const increment =()=>{
-    setCount(count+1)
+    dispatch({
+      type:INCREMENT_COUNT
+    })
   }
 
   const decrement =()=>{
-    setCount(count-1)
+    dispatch({
+      type:DECREMENT_COUNT
+    })
   }
 
   const handleChange =(event) =>{
     const value = parseInt(event.target.value) || 0
-    setValueToAdd(value)
+    
+    dispatch({
+      type: SET_VALUE_TO_ADD,
+      payload : value
+    })
   }
 
   const handleSubmit=(event)=>{
     event.preventDefault()
-    setCount(count + valueToAdd) 
-    setValueToAdd(0)
+
+    dispatch({
+      type:ADD_VALUE_TO_COUNT
+    })
+    // setCount(count + valueToAdd) 
+    // setValueToAdd(0)
   }
 
   return (
     <Panel className='m-3'>
-      <h1 className='text-lg'>Count is {count}</h1>
+      <h1 className='text-lg'>Count is {state.count}</h1>
 
       <div className='flex flex-row'>
       <Button onClick={increment}> Increment</Button>
@@ -41,7 +114,7 @@ function CounterPage({initialCount}) {
       <form onSubmit={handleSubmit}>
         <label>Add a Lot</label>
         <input 
-          value={valueToAdd || ""}
+          value={state.valueToAdd || ""}
           onChange={handleChange} 
           type="number" className='p-1 m-3 bg-gray-50 border border-gray' />
         <Button >Add It</Button>
